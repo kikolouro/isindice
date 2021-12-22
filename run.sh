ce23c4e67112900eb4a003e36299806385b76a7b
Francisco Louro <francisco.louro@infosistema.com>
2021-12-22 10:14:11 +0000

    Fixed run script

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm -d -p 80:80 -p 443:443 --name itindice -v $(pwd)/html:/usr/share/nginx/html nginx -v $(pwd)/app.conf:/etc/nginx/conf.d -v $(pwd)/certs:/certs
+docker run -it --rm -p 80:80 -p 443:443 --name itindice -v "$(pwd)/html:/usr/share/nginx/html" -v $(pwd)/nginx:/etc/nginx/conf.d -v $(pwd)/certs:/certs nginx 
